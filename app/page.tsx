24e3dd0f699e42bc377ae0a40e6bd7091b963f44
Raiaftab <100733123+Raiaftab@users.noreply.github.com>
2025-04-19 16:40:53 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
 
       <footer className="bg-white border-t mt-12 py-6">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <p className="text-center text-gray-500 text-sm">Stock Management System &copy; {new Date().getFullYear()}</p>
+          <p className="text-center text-gray-500 text-sm">Stock Management System Created By Rai Aftab Ameer &copy; {new Date().getFullYear()}</p>
         </div>
       </footer>
     </div>
